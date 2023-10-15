f7d6096f911b7e423ce4418f45ea08b50b96d80a
Lys Irebe <113626927+lysirebe@users.noreply.github.com>
2023-10-15 17:55:59 +0100

    Updating routes to fix page visibility

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,44 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SelectSport from '../pages/SelectSport';
 import Home from '../pages/Home';
-import ConfirmBet from '../pages/confirmBet';
+import ConfirmBet from '../pages/ConfirmBet';
 import Welcome from '../pages/Welcome';
 
 const AppRoutes = () => {
-
-
-  const [selectedSport, setSelectedSport] = useState('americanfootball'); // Default sport is 'football'
-
-  const handleSportChange = (sport) => {
-    setSelectedSport(sport);
-  };
-
   return (
-
-    <Router>
+    <Router basename="/purebet-swipe.io">
       <Routes>
-      < Route path="/purebet-swipe.io" element={<Welcome />} />
+        <Route path="/" element={<Welcome />} />
         <Route path="/selectsport" element={<SelectSport />} />
         <Route path="/home/:sport" element={<Home />} />
         <Route path="/confirmbet" element={<ConfirmBet />} />
       </Routes>
     </Router>
   );
-    
-
-    
 };
 
-
-
-
-
 export default AppRoutes;
-    // <BrowserRouter>
-    //         <Routes>
-    //             <Route path="/start" element={<Start/>} />
-    //             <Route path="/" element={<SelectSport/>} />
-    //             <Route path="/home" element={<Home/>} />
-    //         </Routes>
-    //      </BrowserRouter>
+
+
+
+
